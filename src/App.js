ab9b3649bc326c65699a0b989a2213bcd7b9e21b
Anuj Yadav <arkyyadav001@gmail.com>
2021-05-22 14:11:09 +0530

    made the dictionary app, add dark mode toggler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Container, Switch, withStyles } from "@material-ui/core";
+import Header from "./components/Header";
+import Definitions from "./components/Definitions";
+import { grey, purple } from "@material-ui/core/colors";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en");
+  const [lightMode, setLightMode] = useState(false);
+
+  const DarkMode = withStyles({
+    switchBase: {
+      color: grey[300],
+      "&$checked": {
+        color: grey[500],
+      },
+      "&$checked + $track": {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
+  const dictionaryAPI = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+      );
+
+      // console.log('Fetched Data is: \n');
+      // console.log(data);
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // console.log(meanings);
+
+  useEffect(() => {
+    dictionaryAPI();
+  }, [word, category]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="app"
+      style={{
+        height: "100vh",
+        backgroundColor: lightMode ? "#fff" : "#282c34",
+        color: lightMode ? "black" : "white",
+        transition: "all 0.5s linear",
+      }}
+    >
+      <Container
+        maxWidth="md"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
         >
-          Learn React
-        </a>
-      </header>
+          <span>{lightMode ? "Dark" : "Light"} Mode</span>
+          <DarkMode
+            checked={lightMode}
+            onChange={() => setLightMode(!lightMode)}
+          />
+        </div>
+        <Header
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setWord={setWord}
+          lightMode={lightMode}
+        ></Header>
+        {meanings && (
+          <Definitions
+            word={word}
+            meanings={meanings}
+            category={category}
+            lightMode={lightMode}
+          />
+        )}
+      </Container>
     </div>
   );
 }
